f51a7cd0cfafe1628a777fa2c8c4cfba922dc25f
asyiq <nuruddinasyiq@gmail.com>
2024-01-08 14:16:39 +0800

    IceCream Class Implementation

---
+++ IceCream.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRG2_Assignment
+{
+    abstract class IceCream
+    {
+        private string option;
+        private int scoops;
+        private List<Flavour> flavours;
+        private List<Topping> toppings;
+    }
+}
