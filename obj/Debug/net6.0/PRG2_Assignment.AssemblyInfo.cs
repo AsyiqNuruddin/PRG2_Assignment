111e1c04de3aa869ea39c0564491c662ab5c65c7
Jia Xiang Khoo <s10257702@connect.np.edu.sg>
2024-01-13 00:40:03 +0800

    aw

--- obj/Debug/net6.0/PRG2_Assignment.AssemblyInfo.cs
+++ obj/Debug/net6.0/PRG2_Assignment.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PRG2_Assignment")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97040b1b0b72e376a74623d5bda519a41983ccf9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("PRG2_Assignment")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PRG2_Assignment")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
