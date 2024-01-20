40e5f6c986377d4ca0584fa6c2c0265feed984e6
Jia Xiang Khoo <s10257702@connect.np.edu.sg>
2024-01-20 23:22:47 +0800

    z

--- obj/Release/net6.0/PRG2_Assignment.AssemblyInfo.cs
+++ obj/Release/net6.0/PRG2_Assignment.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PRG2_Assignment")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7959a42573d753bb233da778a931f9d77cb1210c")]
 [assembly: System.Reflection.AssemblyProductAttribute("PRG2_Assignment")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PRG2_Assignment")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
